d9c08e18ca1c22c3cf17002429b6b92d89c3c3f5
Nastassia Ananka <anast.ananko@gmail.com>
2023-04-30 20:41:10 +0200

    feat: add basic header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,49 @@
 import { FC } from 'react';
+import { AppBar, Container, Toolbar, Typography, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+import Logo from '../logo';
 
 import './header.scss';
 
 const Header: FC = () => {
-  return <header className="header">Header</header>;
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#00acc1',
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Logo />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                ml: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'Nunito',
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                color: '#B9F181',
+                textDecoration: 'none',
+              }}
+            >
+              GraphiQL
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
 };
 
 export default Header;
