8923e3502b5aa0223074c2fc6f64f7860c60cc48
Aleksey <aliaksei.balabushka@gmail.com>
2023-05-03 21:26:10 +0300

    feat: add firebase config with sign-in & sign-up forms

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,9 +1,40 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase.ts';
 
 import './sign-in.scss';
 
 const SignIn: FC = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const signIn = (e: FormEvent): void => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => console.log(userCredential))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <form onSubmit={signIn}>
+        <h1>Login</h1>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,9 +1,40 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase.ts';
 
 import './sign-up.scss';
 
 const SignUp: FC = () => {
-  return <div>Registration</div>;
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const signUp = (e: FormEvent): void => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => console.log(userCredential))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <form onSubmit={signUp}>
+        <h1>Create account</h1>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
