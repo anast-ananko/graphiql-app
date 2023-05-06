b16a57cabc220bb783821e1f3baeb08a915dccf2
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-05-06 16:00:55 +0200

    feat: add app errors UIC

--- src/components/explorer/index.tsx
+++ src/components/explorer/index.tsx
@@ -1,11 +1,19 @@
 import { FC } from 'react';
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 import './explorer.scss';
 
+const gridStyle = {
+  item: true,
+  xs: 12,
+  md: 4,
+  lg: 4,
+  xl: 3,
+};
+
 const Explorer: FC = () => {
   return (
-    <Grid item xs={12} md={4} lg={4} xl={3} className="explorer">
+    <Grid {...gridStyle} className="explorer">
       <h1 className="explorer__title">Explorer</h1>
     </Grid>
   );
