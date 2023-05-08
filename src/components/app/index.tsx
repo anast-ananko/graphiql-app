5cd3bdb23f309fe60270eac51bc3e5b3ba62cbbc
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-05-08 15:26:33 +0200

    feat: add app theming

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,15 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
-
+import { ThemeProvider } from '@mui/material/styles';
 import router from '../../router';
 import ErrorsDialog from '../errorsDialog';
 
+import { appTheme } from './app.style';
 import './App.scss';
 
 function App() {
   return (
     <>
-      <ErrorsDialog></ErrorsDialog>
-      <RouterProvider router={router} />;
+      <ThemeProvider theme={appTheme}>
+        <ErrorsDialog></ErrorsDialog>
+        <RouterProvider router={router} />;
+      </ThemeProvider>
     </>
   );
 }
