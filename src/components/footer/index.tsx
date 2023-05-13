68de34d8c37f3c5cc65eea0a72dbb1b44a34423a
Nastassia Ananka <anast.ananko@gmail.com>
2023-05-13 12:14:21 +0200

    feat: add footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,30 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Container } from '@mui/material';
 
+import { containerStyle } from './footer.style';
 import './footer.scss';
 
 const Footer: FC = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <AppBar position="static">
+        <Container style={{ ...containerStyle }} className="footer__container" maxWidth="xl">
+          <div className="footer__links">
+            <Link className="footer__link" to="https://github.com/ablbsk" target="_blank"></Link>
+            <Link className="footer__link" to="https://github.com/ExIxIxS" target="_blank"></Link>
+            <Link
+              className="footer__link"
+              to="https://github.com/anast-ananko"
+              target="_blank"
+            ></Link>
+          </div>
+          <div className="footer__year">2023</div>
+          <div className="footer__logo"></div>
+        </Container>
+      </AppBar>
+    </footer>
+  );
 };
 
 export default Footer;
