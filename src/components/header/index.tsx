fdd7327c041a7cddf79485b726049ea177684f38
Aleksey <aliaksei.balabushka@gmail.com>
2023-05-17 21:10:41 +0300

    refactor: delete unused code

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,67 +1,14 @@
-import React, { FC } from 'react';
-import { AppBar, Button, Typography, Grid } from '@mui/material';
-import i18n from '../../data/i18n';
-import { useTranslation } from 'react-i18next';
+import { FC } from 'react';
+import { AppBar, Button, Grid } from '@mui/material';
 import UserMenu from './userMenu';
-
-import { useAppDispatch, useAppSelector } from '../../hooks/hook';
-import { changeLanguage, selectLang } from '../../store/features/langSlice';
 import ElevationScroll from './elevationScroll';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
-import { updateQuery } from '../../store/features/editorSlice.ts';
-
-import { headerLogoStyle } from './header.style.ts';
-import './header.scss';
 import Language from './language';
+import './header.scss';
 
 const Header: FC = () => {
-  const { t: localize } = useTranslation();
-
-  const dispatch = useAppDispatch();
-
   const { uid } = useSelector((state: RootState) => state.auth);
-  const lang = useAppSelector(selectLang);
-
-  const handleLanguageChange = (): void => {
-    if (lang === 'en') {
-      i18n.changeLanguage('by');
-      dispatch(changeLanguage('by'));
-    } else if (lang === 'by') {
-      i18n.changeLanguage('ru');
-      dispatch(changeLanguage('ru'));
-    } else {
-      i18n.changeLanguage('en');
-      dispatch(changeLanguage('en'));
-    }
-  };
-
-  // return (
-  //   <ElevationScroll>
-  //     <AppBar position="sticky" className="header">
-  //       <Container maxWidth="xl">
-  //         <Toolbar disableGutters>
-  //           <div className="header__container">
-  //             <div className="header__topography">
-  //               <Logo />
-  //               <Typography variant="h5" {...headerLogoStyle} className="header__logo">
-  //                 GraphiQL
-  //               </Typography>
-  //             </div>
-  //             <div className="language">
-  //               <button className="button__lang" onClick={handleLanguageChange}>
-  //                 <span className="language__title">
-  //                   {lang === 'en' ? 'en' : lang === 'by' ? 'by' : 'ru'}
-  //                 </span>
-  //               </button>
-  //             </div>
-  //             {uid && signOutButton}
-  //           </div>
-  //         </Toolbar>
-  //       </Container>
-  //     </AppBar>
-  //   </ElevationScroll>
-  // );
 
   return (
     <ElevationScroll>
