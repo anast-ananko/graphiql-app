2c37ba98d923eeded9a708e3a3b68cf2ae8e45f4
Nastassia Ananka <anast.ananko@gmail.com>
2023-05-18 20:04:25 +0200

    feat: add schema in codemirror

--- src/components/editor/code-mirror/index.tsx
+++ src/components/editor/code-mirror/index.tsx
@@ -1,21 +1,45 @@
-import { FC, useState } from 'react';
+import { FC, useState, memo } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { graphql } from 'cm6-graphql';
+import { buildClientSchema } from 'graphql';
 
 import { ICodeMirror } from '../../../interfaces/code-mirror';
 import { updateQuery, selectQuery } from '../../../store/features/editorSlice';
 import { useAppDispatch, useAppSelector } from '../../../hooks/hook';
+import { useGetGraphQLSchemaQuery } from '../../../store/services/graphQlApi';
 
-const CodeMirrorGraphQL: FC<ICodeMirror> = ({ height }) => {
+const CodeMirrorGraphQL: FC<ICodeMirror> = memo(({ height }) => {
   const [value, setValue] = useState<string>('');
   const query = useAppSelector(selectQuery);
 
   const dispatch = useAppDispatch();
 
-  const onChangeValue = (value: string) => {
+  const { data, isFetching } = useGetGraphQLSchemaQuery();
+  const schema = data && buildClientSchema(data);
+
+  const onChangeValue = (value: string): void => {
     setValue(value);
     dispatch(updateQuery(value));
   };
-  return <CodeMirror value={query} height={`${height}px`} width="100%" onChange={onChangeValue} />;
-};
+
+  return (
+    <>
+      {isFetching && <div className="codemirror__loading">Loading...</div>}
+      {!isFetching && (
+        <CodeMirror
+          value={query}
+          height={`${height}px`}
+          width="100%"
+          onChange={onChangeValue}
+          extensions={[graphql(schema)]}
+          basicSetup={{
+            autocompletion: true,
+            lintKeymap: false,
+          }}
+        />
+      )}
+    </>
+  );
+});
 
 export default CodeMirrorGraphQL;
