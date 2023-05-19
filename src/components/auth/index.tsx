c7173443592a99e43c06ea70cd7d40e19d15adb4
Aleksey <aliaksei.balabushka@gmail.com>
2023-05-19 22:01:14 +0300

    refactor: refactor mui style in auth component

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,44 +1,36 @@
 import { FC } from 'react';
-import { Container, Grid, Typography, Link } from '@mui/material';
 import { AuthType } from '../../types';
+import { Container, Grid, Typography, Link } from '@mui/material';
+import { authContainer, authImage, authLink, authRedirectLink } from './auth.style.ts';
 import './auth.scss';
 
 const Auth: FC<AuthType> = ({ form, isLogin }) => {
   return (
     <div className="auth">
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        className="not-found__container"
-        rowSpacing={{ xs: 4, sm: 6 }}
-        minWidth={{ md: '700px' }}
-        sx={{ padding: { xs: '0 10px', sm: '0 30px' } }}
-      >
+      <Grid className="auth__container" {...authContainer}>
         <Grid item>
           <Typography variant="h2">{isLogin ? 'Login' : 'Create account'}</Typography>
         </Grid>
         <Grid item>{form}</Grid>
-        <Grid item container justifyContent="center">
+        <Grid {...authRedirectLink}>
           {isLogin ? (
             <Typography variant="body1">
               If you don`t have account,{' '}
-              <Link href="/sign-up" color="secondary" underline="none">
+              <Link href="/sign-up" {...authLink}>
                 create account
               </Link>
             </Typography>
           ) : (
             <Typography variant="body1">
               If you have account please{' '}
-              <Link href="/sign-in" color="secondary" underline="none">
+              <Link href="/sign-in" {...authLink}>
                 login
               </Link>
             </Typography>
           )}
         </Grid>
       </Grid>
-      <Container sx={{ display: { xs: 'none', md: 'block' } }}>
+      <Container {...authImage}>
         <img className="auth__img" src="auth.png" alt="" />
       </Container>
     </div>
