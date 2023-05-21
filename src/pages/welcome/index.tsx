5d5c165b23170cfb9656da0842057525e73c881c
Nastassia Ananka <anast.ananko@gmail.com>
2023-05-21 14:41:34 +0200

    feat: add language for welcome page and delete unused files

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import { RootState } from '../../store';
 import WelcomeAccordion from './welcomeAccordion';
 import WelcomeSlider from './welcomeSlider';
@@ -15,21 +16,23 @@ import {
 const Welcome: FC = () => {
   const { uid } = useSelector((state: RootState) => state.auth);
 
+  const { t: localize } = useTranslation();
+
   return (
     <Grid {...welcomeContainer}>
       <Grid {...welcomeHeader}>
-        <Typography variant="h2">Welcome to StarWars GraphQl far, far away</Typography>
+        <Typography variant="h2">{localize('welcome.text')}</Typography>
       </Grid>
       {uid && (
         <Grid {...welcomeButtonContainer}>
           <Button href="/main" variant="outlined">
-            Open main page
+            {localize('welcome.button')}
           </Button>
         </Grid>
       )}
       <Grid {...welcomeSection}>
         <Grid {...welcomeSectionHeader}>
-          <Typography variant="h3">About</Typography>
+          <Typography variant="h3">{localize('welcome.about')}</Typography>
         </Grid>
         <Grid item>
           <WelcomeAccordion />
@@ -37,7 +40,7 @@ const Welcome: FC = () => {
       </Grid>
       <Grid {...welcomeSection}>
         <Grid {...welcomeSectionHeader}>
-          <Typography variant="h3">Developers</Typography>
+          <Typography variant="h3">{localize('welcome.developers')}</Typography>
         </Grid>
         <Grid item>
           <WelcomeSlider />
--- src/pages/welcome/welcomeAccordion/index.tsx
+++ src/pages/welcome/welcomeAccordion/index.tsx
@@ -1,10 +1,13 @@
 import { FC, useState, SyntheticEvent } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { welcomeAccordionText, welcomeAccordionArticleHeader } from './welcomeArrodion.style.ts';
+import { welcomeAccordionText, welcomeAccordionArticleHeader } from './welcomeAccordion.style.ts';
 
 const WelcomeAccordion: FC = () => {
   const [expanded, setExpanded] = useState<string | false>(false);
+
+  const { t: localize } = useTranslation();
 
   const handleChange = (panel: string) => (_: SyntheticEvent, isExpanded: boolean) => {
     setExpanded(isExpanded && panel);
@@ -14,59 +17,35 @@ const WelcomeAccordion: FC = () => {
     <>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography {...welcomeAccordionArticleHeader}>Project</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>GraphQl StarWars Explorer</Typography>
+          <Typography {...welcomeAccordionArticleHeader}>
+            {localize('welcomeAccordion.project')}
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {localize('welcomeAccordion.text-1')}
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography {...welcomeAccordionText}>
-            Introducing the GraphQl StarWars Explorer project, a delightful journey where developers
-            can traverse the vast reaches of the Star Wars universe with just a few keystrokes!
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            Picture yourself wielding the power of GraphQL, the Jedi Knight of API query languages,
-            as you delve into a galaxy filled with spaceships, droids, and lightsabers. With this
-            project, you`ll feel like a Jedi librarian, effortlessly navigating through a treasure
-            trove of Star Wars data.
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            Explore characters like Luke Skywalker, Darth Vader, and Yoda with the grace of a
-            Corellian smuggler, seamlessly traversing their connections, from their affiliations to
-            their iconic quotes. Want to know which films R2-D2 made an appearance in? Simply summon
-            the Force of GraphQL to retrieve the answer in an instant. But that`s not all!
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            This project offers more twists than a Hutt`s tail. It allows you to unearth details
-            about starships, planets, species, and even the ever-popular opening crawl of each Star
-            Wars movie. You`ll feel like a protocol droid fluent in over six million forms of
-            entertainment knowledge.
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            So, embrace the GraphQl StarWars Explorer project, where coding meets the Force. It`s a
-            whimsical adventure that will have you saying, `May the queries be with you` and
-            laughing like a jovial Wookiee in no time.
-          </Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-2')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-3')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-4')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-5')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-6')}</Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography {...welcomeAccordionArticleHeader}>Course</Typography>
-          <Typography sx={{ color: 'text.secondary' }}>RS School React Course</Typography>
+          <Typography {...welcomeAccordionArticleHeader}>
+            {localize('welcomeAccordion.course-1')}
+          </Typography>
+          <Typography sx={{ color: 'text.secondary' }}>
+            {localize('welcomeAccordion.course-2')}
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography {...welcomeAccordionText}>React Course is free and online!</Typography>
-          <Typography {...welcomeAccordionText}>
-            The RS School React course is working by the principle of pay it forward. Members of our
-            community share their knowledge and check students tasks for free. And we hope our
-            students will continue this work as our mentors in the future.
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            Everyone can study at RS School, regardless of age, professional employment, or place of
-            residence. However, you should have sufficient base knowledge before the program begins.
-          </Typography>
-          <Typography {...welcomeAccordionText}>
-            After accomplishing an education, students will receive an electronic certificate of
-            completion.
-          </Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.course-3')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-7')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-8')}</Typography>
+          <Typography {...welcomeAccordionText}>{localize('welcomeAccordion.text-9')}</Typography>
         </AccordionDetails>
       </Accordion>
     </>
