ed58f8e21c15174b0dadd027f8275cd8a59f4bf6
Nastassia Ananka <anast.ananko@gmail.com>
2023-05-21 21:09:56 +0200

    feat: add translation

--- src/utils/validateHeaders.ts
+++ src/utils/validateHeaders.ts
@@ -1,53 +1,65 @@
 import { IValidatedHeaders, IHeadersValidationMap } from '../interfaces/validatedHeaders';
 import { validTypes } from '../constants/validHeaderTypes';
 
-const validateAuthorization = (authorizationHeader: string): string => {
+const validateAuthorization = (
+  authorizationHeader: string,
+  localize: (value: string) => string
+): string => {
   if (!authorizationHeader.startsWith('Bearer')) {
-    return 'Header Authorization must start with Bearer';
+    return `${localize('error.text-5')}`;
   } else {
     const token = authorizationHeader.replace('Bearer ', '');
 
     if (token.length <= 10) {
-      return 'Token in header Authorization must be longer then 10 symbols';
+      return `${localize('error.text-6')}`;
     }
     if (token.length >= 20) {
-      return 'Token in header Authorization must be shorter then 20 symbols';
+      return `${localize('error.text-7')}`;
     }
   }
 
   return '';
 };
 
-const validateAccessControlAllowOrigin = (header: string): string => {
+const validateAccessControlAllowOrigin = (
+  header: string,
+  localize: (value: string) => string
+): string => {
   const originRegex =
     /^(\*|(https?|ftp|file):\/\/([a-zA-Z0-9]+)(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})|null)$/i;
 
-  return !originRegex.test(header) ? 'Invalid header Access-Control-Allow-Origin' : '';
+  return !originRegex.test(header) ? `${localize('error.text-8')}` : '';
 };
 
-const validateAccessControlAllowCredentials = (header: string): string => {
+const validateAccessControlAllowCredentials = (
+  header: string,
+  localize: (value: string) => string
+): string => {
   const headerValue = header.toLowerCase().trim();
 
   if (headerValue !== 'true' && headerValue !== 'false') {
-    return 'Invalid header Access-Control-Allow-Credentials';
+    return `${localize('error.text-9')}`;
   }
 
   return '';
 };
 
-const validateAccept = (header: string): string => {
+const validateAccept = (header: string, localize: (value: string) => string): string => {
   const requestedTypes = header.split(',');
 
   for (const type of requestedTypes) {
     if (!validTypes.includes(type.trim())) {
-      return 'Invalid header Accept';
+      return `${localize('error.text-10')}`;
     }
   }
 
   return '';
 };
 
-const validateHeaders = (value: IValidatedHeaders): string[] => {
+const validateHeaders = (
+  value: IValidatedHeaders,
+  localize: (value: string) => string
+): string[] => {
   if (!value) {
     return [];
   }
@@ -65,7 +77,7 @@ const validateHeaders = (value: IValidatedHeaders): string[] => {
     const headerValue = value[headerName];
     if (headerValue) {
       const validationFn = headersValidationMap[headerName];
-      const error = validationFn(headerValue);
+      const error = validationFn(headerValue, localize);
       if (error) {
         errors.push(error);
       }
