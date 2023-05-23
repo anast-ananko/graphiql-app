7d59f7b6c81cb25c8a77c0a9097fa834bcdf9e08
Nastassia Ananka <anast.ananko@gmail.com>
2023-05-23 20:24:35 +0200

    fix: fix height

--- src/pages/main/main.style.ts
+++ src/pages/main/main.style.ts
@@ -2,7 +2,7 @@ const gridMainContainerStyle = {
   container: true,
   spacing: 2,
   maxWidth: { xs: '300px', sm: '520px', md: '880px', lg: '1240px', xl: '1500px' },
-  height: { xs: '100%', lg: 'calc(100vh - 146px)' },
+  height: { xs: '100%', lg: 'calc(100vh - 190px)' },
   sx: { m: '10px auto 20px' },
 };
 
